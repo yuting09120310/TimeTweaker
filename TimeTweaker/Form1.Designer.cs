d647248efc859ccc8e88d60076ad806601a8d83e
YuTing09120310 <YuTing09120310@DP_652>
2024-10-25 10:58:16 +0800

    加入專案檔案。

---
+++ TimeTweaker/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TimeTweaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBrowseFolder = new System.Windows.Forms.TextBox();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtBrowseFolder
+            // 
+            this.TxtBrowseFolder.Location = new System.Drawing.Point(120, 47);
+            this.TxtBrowseFolder.Name = "TxtBrowseFolder";
+            this.TxtBrowseFolder.Size = new System.Drawing.Size(212, 22);
+            this.TxtBrowseFolder.TabIndex = 0;
+            this.TxtBrowseFolder.Click += new System.EventHandler(this.TxtBrowseFolder_Click);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.Location = new System.Drawing.Point(136, 150);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(89, 35);
+            this.BtnUpdate.TabIndex = 1;
+            this.BtnUpdate.Text = "Update";
+            this.BtnUpdate.UseVisualStyleBackColor = true;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd HH:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(120, 98);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(212, 22);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(26, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "FolderPath";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(15, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "UpdateTime";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 221);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.TxtBrowseFolder);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TimeTweaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBrowseFolder;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TimeTweaker/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTweaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtBrowseFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                TxtBrowseFolder.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            string folderPath = TxtBrowseFolder.Text;
+            if (Directory.Exists(folderPath))
+            {
+                try
+                {
+                    // 遞迴修改資料夾和子資料夾內所有檔案的時間
+                    UpdateFolderTimes(folderPath);
+                    MessageBox.Show("所有檔案的時間已成功更新！");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"修改檔案時間時發生錯誤：{ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("資料夾不存在！");
+            }
+        }
+
+        private void UpdateFolderTimes(string folderPath)
+        {
+            // 獲取該資料夾內的所有檔案
+            string[] files = Directory.GetFiles(folderPath);
+            foreach (var file in files)
+            {
+                try
+                {
+                    // 修改每個檔案的時間屬性
+                    File.SetCreationTime(file, dateTimePicker1.Value);
+                    File.SetLastWriteTime(file, dateTimePicker1.Value);
+                    File.SetLastAccessTime(file, dateTimePicker1.Value);
+                }
+                catch (Exception ex)
+                {
+                    // 顯示錯誤但不停止迴圈
+                    MessageBox.Show($"修改檔案時間時發生錯誤：{file}\n錯誤訊息：{ex.Message}");
+                }
+            }
+
+            // 獲取該資料夾內的所有子資料夾
+            string[] subFolders = Directory.GetDirectories(folderPath);
+            foreach (var subFolder in subFolders)
+            {
+                // 遞迴處理每個子資料夾
+                UpdateFolderTimes(subFolder);
+            }
+        }
+    }
+}
